17895f7e6840e6626234e1ed22fb4a56c9d15572
Seth Shelnutt <seth@tiledb.io>
2023-01-23 08:14:34 -0500

    WIP

---
+++ recipe/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -exo pipefail
+
+mkdir libtiledbsoma-build && cd libtiledbsoma-build
+cmake \
+  -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+  -DOVERRIDE_INSTALL_PREFIX=OFF \
+  -DCMAKE_BUILD_TYPE=Release \
+  ../libtiledbsoma
+
+make -j ${CPU_COUNT}
