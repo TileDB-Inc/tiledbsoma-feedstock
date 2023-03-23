76731be3c5f511aef7368e43720da7f145f667df
Seth Shelnutt <seth@tiledb.io>
2023-03-23 15:11:26 -0400

    Update build-libtiledbsoma.sh

--- recipe/build-libtiledbsoma.sh
+++ recipe/build-libtiledbsoma.sh
@@ -3,6 +3,13 @@
 set -exo pipefail
 
 mkdir libtiledbsoma-build && cd libtiledbsoma-build
-cmake -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" ../libtiledbsoma
+
+cmake \
+  -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+  -DOVERRIDE_INSTALL_PREFIX=OFF \
+  -DCMAKE_BUILD_TYPE=Release \
+  ../libtiledbsoma
+
 make -j ${CPU_COUNT}
+
 make install-libtiledbsoma
