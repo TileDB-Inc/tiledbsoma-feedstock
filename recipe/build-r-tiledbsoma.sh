182dbf961ffda52b2e144a410f03423b893b13ab
John Blischak <jdblischak@gmail.com>
2023-05-08 15:16:00 -0400

    Try conda-forge solution for missing symbols on osx-64
    https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk

--- recipe/build-r-tiledbsoma.sh
+++ recipe/build-r-tiledbsoma.sh
@@ -6,6 +6,11 @@ cd apis/r
 
 export DISABLE_AUTOBREW=1
 
+# https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+if [[ $target_platform  == osx-64 ]]; then
+  CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 export CXX17FLAGS="-Wno-deprecated-declarations -Wno-deprecated"
 
 ${R} CMD INSTALL --build . ${R_ARGS}
