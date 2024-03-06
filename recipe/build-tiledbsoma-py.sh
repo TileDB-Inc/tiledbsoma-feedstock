ac62e5989e9cfc2adbd94f07cded290433e27d75
John Kerl <kerl.john.r@gmail.com>
2024-03-06 14:15:52 -0500

    more advice from @jdblischak

--- recipe/build-tiledbsoma-py.sh
+++ recipe/build-tiledbsoma-py.sh
@@ -5,6 +5,9 @@ set -ex
 cd apis/python
 
 echo "$PKG_VERSION" >> RELEASE-VERSION
-$PYTHON setup.py install --single-version-externally-managed --record record.txt --libtiledbsoma="${PREFIX}"
+
+export TILEDB_PATH=$PREFIX
+export TILEDBSOMA_PATH=$PREFIX
+$PYTHON -m pip install --no-deps --no-build-isolation . -vv
 
 $PYTHON setup.py clean --all
