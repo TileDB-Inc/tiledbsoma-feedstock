4651e8cf4542cf97f8d6725392d222ce7867a0b4
XanthosXanthopoulos <xanxanthopoulos@gmail.com>
2025-01-09 17:42:38 +0200

    Export env variables for all mac targets

--- recipe/build-r-tiledbsoma.sh
+++ recipe/build-r-tiledbsoma.sh
@@ -10,7 +10,7 @@ export CXXFLAGS=""
 export DISABLE_AUTOBREW=1
 
 # https://github.com/conda-forge/r-tiledb-feedstock/commit/29cb6816636e7b5b58545e1407a8f0c29ff9dc39
-if [[ $target_platform  == osx-64 ]]; then
+if [[ $target_platform == osx-* ]]; then
   export NN_CXX_ORIG=$CXX
   export NN_CC_ORIG=$CC
   export CXX=$RECIPE_DIR/cxx_wrap.sh
