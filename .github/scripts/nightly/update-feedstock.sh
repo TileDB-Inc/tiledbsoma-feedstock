e30d0066a74fab2da19c95f797734f657d32fbfc
John Blischak <jdblischak@gmail.com>
2025-01-20 16:02:08 -0500

    [ci skip] Apply cpp20 fixes from Xanthos to nightly build

--- .github/scripts/nightly/update-feedstock.sh
+++ .github/scripts/nightly/update-feedstock.sh
@@ -16,5 +16,9 @@ sed -i \
   s/"tiledb rc"/"tiledb nightlies"/ \
   recipe/conda_build_config.yaml
 
+# Apply patch from Xanthos to support C++20
+# https://github.com/TileDB-Inc/tiledbsoma-feedstock/pull/246
+patch -p1 < .github/scripts/nightly/cpp20.patch
+
 # Print differences
 git --no-pager diff conda-forge.yml recipe/conda_build_config.yaml
