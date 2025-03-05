c75be7a9dac8760347a1820b86c41c33e8bb21fe
John Kerl <kerl.john.r@gmail.com>
2025-03-05 20:59:55 +0000

    more

--- recipe/build-libtiledbsoma.sh
+++ recipe/build-libtiledbsoma.sh
@@ -13,6 +13,7 @@ cmake \
   -DCMAKE_BUILD_TYPE=Release \
   -DTILEDBSOMA_BUILD_CLI=OFF \
   -DTILEDBSOMA_ENABLE_TESTING=OFF \
+  -DSPDLOG_LINK_SHARED=ON \
   ../libtiledbsoma
 
 make -j ${CPU_COUNT}
