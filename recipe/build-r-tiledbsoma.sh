59d5a2e03e17966ace0c1b21ec0d065e55abf4f0
XanthosXanthopoulos <xanxanthopoulos@gmail.com>
2025-03-26 18:17:10 +0200

    Run strip down version with forked rcppspdlog [skip ci]

--- recipe/build-r-tiledbsoma.sh
+++ recipe/build-r-tiledbsoma.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+git clone https://github.com/XanthosXanthopoulos/rcppspdlog.git
+cd rcppspdlog
+R CMD build --no-build-vignettes --no-manual .
+R CMD INSTALL $(ls -1tr *.tar.gz | tail -1)
+cd ../
+
 cd apis/r
 
 export DISABLE_AUTOBREW=1
