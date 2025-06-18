d6ce419e42d415e54640f4c2d3f2d8a886515f07
John Blischak <jdblischak@gmail.com>
2025-06-18 13:19:43 -0400

    [ci skip] Bump somacore to 1.0.29 for nightly feedstock builds

--- .github/scripts/nightly/update-recipe.sh
+++ .github/scripts/nightly/update-recipe.sh
@@ -20,4 +20,9 @@ sed -i \
   s/"sha256: .\+"/"git_rev: main\n  git_depth: -1"/ \
   recipe/meta.yaml
 
+# (Temporary) Bump somacore
+sed -i \
+  s/"somacore ==.\+"/"somacore ==1.0.29"/ \
+  recipe/meta.yaml
+
 git --no-pager diff recipe/meta.yaml
